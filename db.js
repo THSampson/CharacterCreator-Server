7517ef6a2aefe98069f06f35b970bc67f14c1261
Taylor Sampson <thsampsn@gmail.com>
2020-09-14 10:27:42 -0400

    fix db code

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 
-    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/CharacterCreator` {
+    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/CharacterCreator`, {
       dialect: 'postgres'
 });
 
